e618c5cf2d682c4007154e3c74ac1a9a82b7ed43
Konrad Kotyla <kotyla.konrad@gmail.com>
2020-10-21 01:25:38 +0200

    Biblioteka v0.4

--- src/Library.java
+++ src/Library.java
@@ -1,6 +1,6 @@
 public class Library {
     public static void main(String[] args) {
-        final String appName = "Biblioteka v0.3";
+        final String appName = "Biblioteka v0.4";
 
         Book book1 = new Book("W pustyni i w puszczy", "Henryk Sienkiewicz", 2010,
                 296, "Greg", "9788373271890");
@@ -9,7 +9,7 @@ public class Library {
                 "9788324620845");
         Book book3 = new Book("SCJP Sun Certified Programmer for Java 6 Study Guide",
                 "Bert Bates, Katherine Sierra", 2008, 851,
-                "McGraw-Hill Osborne Media", "9780071591065");
+                "McGraw-Hill Osborne Media");
 
         System.out.println(appName);
         System.out.println("Książki dostępne w bibliotece:");
