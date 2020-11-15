103d55c1936c48772cceb59feb975d5f7ba63397
Konrad Kotyla <kotyla.konrad@gmail.com>
2020-11-15 23:25:07 +0100

    Biblioteka v1.1

--- src/pl/javastart/library/app/LibraryControl.java
+++ src/pl/javastart/library/app/LibraryControl.java
@@ -1,13 +1,16 @@
-package pjavastart.library.app;
+package pl.javastart.library.app;
 
-import pjavastart.library.io.DataReader;
-import pjavastart.library.model.Book;
-import pjavastart.library.model.Library;
+import pl.javastart.library.io.DataReader;
+import pl.javastart.library.model.Book;
+import pl.javastart.library.model.Library;
+import pl.javastart.library.model.Magazine;
 
 public class LibraryControl {
     private static final int EXIT = 0;
     private static final int ADD_BOOK = 1;
-    private static final int PRINT_BOOKS = 2;
+    private static final int ADD_MAGAZINES = 2;
+    private static final int PRINT_BOOKS = 3;
+    private static final int PRINT_MAGAZINES = 4;
 
     DataReader dataReader = new DataReader();
     Library library = new Library();
@@ -26,8 +29,14 @@ public class LibraryControl {
                 case ADD_BOOK:
                     addBook();
                     break;
+                case ADD_MAGAZINES:
+                    addMagazines();
+                    break;
                 case PRINT_BOOKS:
                     printBooks();
+                    break;
+                case PRINT_MAGAZINES:
+                    printMagazines();
                     break;
                 default:
                     System.out.println("Nie znaleziono takiej opcji.");
@@ -39,7 +48,9 @@ public class LibraryControl {
         System.out.println("Wybierz jedną z opcji: ");
         System.out.println(EXIT + " - wyjście z programu");
         System.out.println(ADD_BOOK + " - dodaj książkę");
+        System.out.println(ADD_MAGAZINES + " - dodaj magazyn");
         System.out.println(PRINT_BOOKS + " - wyświetl dostępne książki");
+        System.out.println(PRINT_MAGAZINES + " - wyświetl dostępne magazyny");
     }
 
     private void exit() {
@@ -54,5 +65,14 @@ public class LibraryControl {
 
     private void printBooks() {
         library.printBooks();
+    }
+
+    private void addMagazines() {
+        Magazine magazine = dataReader.readAndCreateMagazine();
+        library.addMagazine(magazine);
+    }
+
+    private void printMagazines() {
+        library.printMagazines();
     }
 }
