cd5840c4ce7c262356f2ca9fbd8eaa942690a49a
Konrad Kotyla <kotyla.konrad@gmail.com>
2020-11-26 00:35:16 +0100

    Biblioteka v1.2

--- src/pl/javastart/library/model/Book.java
+++ src/pl/javastart/library/model/Book.java
@@ -7,9 +7,7 @@ public class Book extends Publication {
 
 
     public Book(String title, String author, int year, int pages, String publisher, String isbn) {
-        setTitle(title);
-        setYear(year);
-        setPublisher(publisher);
+        super(year, title, publisher);
         this.author = author;
         this.pages = pages;
         this.isbn = isbn;
