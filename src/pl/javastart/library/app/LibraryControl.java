04260b23c428c4813184810cb82057a61ad595da
kkotyla <konrad.kotyla@capgemini.com>
2021-09-01 12:38:09 +0200

    Biblioteka v1.7

--- src/pl/javastart/library/app/LibraryControl.java
+++ src/pl/javastart/library/app/LibraryControl.java
@@ -102,4 +102,33 @@ public class LibraryControl {
 		}
 		return option;
 	}
+
+	private enum Option {
+		EXIT(0, "wyjście z programu"),
+		ADD_BOOK(1, "Dodaj książkę"),
+		ADD_MAGAZINES(2, "Dodaj magazyn"),
+		PRINT_BOOKS(3, "Wyświetl dostępne książki"),
+		PRINT_MAGAZINES(4, "Wyświetl dostępne magazyny");
+
+		private final int value;
+		private final String description;
+
+		Option(int value, String description) {
+			this.value = value;
+			this.description = description;
+		}
+
+		static Option createFromInt(int option) throws NoSuchOptionException {
+			try {
+				return Option.values()[option];
+			} catch (ArrayIndexOutOfBoundsException exception) {
+				throw new NoSuchOptionException("Wybrano niepoprawny numer! - " + option);
+			}
+		}
+
+		@Override
+		public String toString() {
+			return value + " - " + description;
+		}
+	}
 }
